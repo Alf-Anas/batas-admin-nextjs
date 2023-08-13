555ec946c738f4213720c40e8726fd8e29ee0c15
Alf-Anas <geoit.dev@gmail.com>
2023-08-13 23:14:59 +0700

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
 // import "antd/dist/reset.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
